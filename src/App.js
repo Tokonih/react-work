3bf20a5a2b3b575eed20936c5d875755b7e4c659
TOKONIH <tokonihstandwell@gmail.com>
2023-06-02 21:40:40 +0100

    testing React

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Navigation from "./Component/Navigation";
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Navigation.js
@@ -0,0 +1,11 @@
+
+
+function Navigation(){
+    return(
+        <div>
+            <h1>this is working</h1>
+        </div>
+    )
+}
+
+export default Navigation;
