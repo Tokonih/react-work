00aa8fabcfb176476896b41a79971fe2ef7d9645
TOKONIH <tokonihstandwell@gmail.com>
2023-06-05 15:38:23 +0100

    react

--- src/Component/Section1.js
+++ src/Component/Section1.js
@@ -3,23 +3,21 @@ import "./Section1.css";
 function Section1() {
   return (
     <div className="Approach">
-      <div>
-        <div className="Approach-">
-          <h1>Our Approach</h1>
-          <h3>
-            DESIGNING OUR EASYIVY PANELS TO THE HIGHEST STANDARDS IN OUR OWN
-            FACILITY
-          </h3>
-          <p>
-            EASYIVY is the only South African foliage brand that uses UV
-            blocking technology with strong colourfast pigments to create
-            inherently fade resistant foliage for outdoor use. Our products 100%
-            maintenance free and fade resistant
-          </p>
-          <button>SEE OUR PROJECTS</button>
-        </div>
+      <div className="app">
+        <h1>Our Approach</h1>
+        <h3>
+          DESIGNING OUR EASYIVY PANELS TO THE HIGHEST STANDARDS IN OUR OWN
+          FACILITY
+        </h3>
+        <p>
+          EASYIVY is the only South African foliage brand that uses UV blocking
+          technology with strong colourfast pigments to create inherently fade
+          resistant foliage for outdoor use. Our products 100% maintenance free
+          and fade resistant
+        </p>
+        <button>SEE OUR PROJECTS</button>
       </div>
-      <div className="grass-image"> 
+      <div className="grass-image">
         <img src={img} alt="" />
       </div>
     </div>
